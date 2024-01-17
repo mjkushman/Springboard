19ab02313c5cb3e90ae00a174eb3d2f54b96cc47
Michael Kushman <michaelkushman@MacBook-Pro.local>
2024-01-16 16:48:55 -0800

    Added the first React Router exercise. Very simple linking and routing

---
+++ Unit 41 React Router/exercise/vending-machine/src/Cookies.jsx
@@ -0,0 +1,12 @@
+
+const Cookies = () => {
+  return (
+  <>Have some Cookies
+    <div>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f1/2ChocolateChipCookies.jpg" width="500"/>
+      </div>
+  </>
+  )
+};
+
+export default Cookies;
---
+++ Unit 41 React Router/exercise/vending-machine/src/Spam.jsx
@@ -0,0 +1,11 @@
+const Spam = () => {
+  return(
+  <>
+    Put more SPAM in your life
+    <div>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/31/Homemade_Spam_Musubi.jpg" width="500"/>
+    </div>
+  </>)
+};
+
+export default Spam;
